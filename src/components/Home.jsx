3c7a6cc75cfc86a1c66dd7870df389879e102cdc
alfonsovm13 <alfonsovengoechea@gmail.com>
2024-07-07 19:26:47 -0500

    CambiosEnHome

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>Home</div>
+      <p>HOLA SOY</p>
+    </>
+  );
+};
 
-export default Home
+export default Home;
